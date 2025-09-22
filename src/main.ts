7d9087eedca2e02bb72835513992e95384f99772
Helen Virtanen <helenvirtanen28@gmail.com>
2025-09-22 20:08:48 +0300

    feat(main.ts): add check of model Catalog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 import './scss/styles.scss';
+import { Catalog } from './components/Models/Catalog';
+import { apiProducts } from './utils/data';
+
+const productsModel = new Catalog();
+productsModel.setProducts(apiProducts.items);
+
+console.log("Массив товаров из каталога: ", productsModel.getProducts());
