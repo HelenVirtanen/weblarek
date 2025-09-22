11adbe7415b7d5369cb362b958f67d79c75bd60e
Helen Virtanen <helenvirtanen28@gmail.com>
2025-09-22 19:20:54 +0300

    feat(models): add Сatalog, Cart, and Product classes
    - created three files for each class in folder Models
    - defined main properties and methods for catalog functionality

---
+++ src/components/Models/Catalog.ts
@@ -0,0 +1,22 @@
+import { IProduct } from "../../types";
+
+export class Catalog {
+  productsList: IProduct[] = [];
+  selectedProduct: IProduct | null = null; 
+
+  getProducts(): IProduct[] {
+    return this.productsList;
+  };
+
+  getProductById(id: string): IProduct | undefined {
+    return this.productsList.filter(product => product.id === id)[0];
+  }
+  
+  saveProductToShow(id: string): void {
+    this.selectedProduct = this.productsList.filter(product => product.id === id)[0];
+  }
+
+  getProductToShow(): IProduct | null {
+    return this.selectedProduct;
+  }
+}
